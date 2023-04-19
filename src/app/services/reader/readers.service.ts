54ab0a72824ae25b4c297834b8a9d202414beb88
josue <josueaguilarjnai@gmail.com>
2023-04-18 21:38:35 -0600

    Create all the files and the structure to start developíng readers secction

--- src/app/services/reader/readers.service.ts
+++ src/app/services/reader/readers.service.ts
@@ -1,9 +1,14 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observer} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReadersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  callGetAllReaders(): Observer<Reader>
 }
