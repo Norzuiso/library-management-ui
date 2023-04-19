4512abce2c5c1e41090bb8147e362bd9856f34a3
josue <josueaguilarjnai@gmail.com>
2023-04-19 15:23:43 -0600

    Created loan service/endpoints consumer

--- src/app/services/loan/loan.service.ts
+++ src/app/services/loan/loan.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {LoanEntity} from "../../entities/loan/loan-entity";
+import {globalConstants} from "../../utils/global.constants";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoanService {
 
-  constructor() { }
+  constructor(private client: HttpClient) {
+  }
+
+  callGetAllLoans(): Observable<LoanEntity[]> {
+    return this.client.get<LoanEntity[]>(`${globalConstants.LOCALHOST_8020_BUSINESS}/loan`)
+  }
+  callGetLoanById(id: number): Observable<LoanEntity> {
+    return this.client.get<LoanEntity>(`${globalConstants.LOCALHOST_8020_BUSINESS}/loan/${id}`)
+  }
+
+  callCreateLoan(loan: LoanEntity):Observable<LoanEntity>{
+    return this.client.post<LoanEntity>(`${globalConstants.LOCALHOST_8020_BUSINESS}/loan`, loan);
+  }
+
+
+  callDeliveryLoan(loan: LoanEntity):Observable<LoanEntity>{
+    return this.client.post<LoanEntity>(`${globalConstants.LOCALHOST_8020_BUSINESS}/loan/back`, loan);
+  }
+
 }
